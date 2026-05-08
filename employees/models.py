84b2c601e05a37718b456ce68183f509f06e58ac
ggozde0 <ggozde.alan@gmail.com>
2026-05-08 13:50:05 +0300

    Update models.py

--- employees/models.py
+++ employees/models.py
@@ -3,20 +3,20 @@ from django.db import models
 # Personel rollerini seçenek olarak tanımlıyoruz
 ROLE_CHOICES = [
     ('admin', 'Yönetici'),
-    ('employee', 'Çalışan'),
+    ('student', 'Ogrenci'),
 ]
 
-class Employee(models.Model):
+class Student(models.Model):
     first_name = models.CharField(max_length=50, verbose_name="Ad")
     last_name = models.CharField(max_length=50, verbose_name="Soyad")
     # Her çalışanın benzersiz bir ID'si olmalı
-    emp_id = models.CharField(max_length=20, unique=True, verbose_name="Personel ID")
+    student_id = models.CharField(max_length=20, unique=True, verbose_name="Student ID")
     
     # Çalışanın yüzünü tanımamız için fotoğrafını yükleyeceğiz
     # 'profile_images/' klasörüne kaydedilecek
     photo = models.ImageField(upload_to='profile_images/', verbose_name="Profil Fotoğrafı")
     
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='employee')
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='student')
     
     # Hangi departmanda çalışıyor?
     department = models.CharField(max_length=50, blank=True, null=True)
@@ -25,4 +25,4 @@ class Employee(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} ({self.emp_id})"
+        return f"{self.first_name} {self.last_name} ({self.student_id})"
