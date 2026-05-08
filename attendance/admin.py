05b1ff33e5324d0f2d16665d14e1803a37343470
ggozde0 <ggozde.alan@gmail.com>
2026-05-08 10:55:18 +0300

    Update admin.py

--- attendance/admin.py
+++ attendance/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Attendance
 
 class AttendanceAdmin(admin.ModelAdmin):
-    list_display = ('employee', 'date', 'time_in', 'time_out')
+    list_display = ('student', 'date', 'time_in', 'time_out')
     list_filter = ('date',) # Tarihe göre filtreleme özelliği ekler
 
 admin.site.register(Attendance, AttendanceAdmin)
